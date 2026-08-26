70ab70af37e62c91e344718d8d37a5c1f02dbc91
Kaleka-kal <itochnohz@gmail.com>
2026-08-26 21:58:00 +0300

    Update token.java

--- src/main/java/com/example/demo/token.java
+++ src/main/java/com/example/demo/token.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 public class token {
-    static final String token = "8569197025:AAFjNm6uE8jd0_TskrzrvgegUkroWTCY1D0";
+    static final String token = "";
     //token бота, его можно запросить у @botfather(ну или как он там)
     public static String getToken() {
         return token;
